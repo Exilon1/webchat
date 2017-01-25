5ab405445437e09e3af8f3b404a7d8b0bb5b3e76
Alexey <delexyz@mail.ru>
2017-01-25 23:34:57 +0400

    Inicial

---
+++ src/main/java/Controller.java
@@ -0,0 +1,7 @@
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Created by Alexey on 25.01.2017.
+ */
+public class Controller extends HttpServlet {
+}
---
+++ src/main/java/Hello.java
@@ -0,0 +1,10 @@
+package main.java;
+
+/**
+ * Created by Dima on 17.01.2017.
+ */
+public class Hello {
+    public String printHello() {
+        return "Hellooooo!!!!";
+    }
+}
