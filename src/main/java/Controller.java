c8ac0efc706e4321b81e9fe2998b36c97f7f3ac7
Alexey <delexyz@mail.ru>
2017-01-30 16:40:01 +0400

    Development

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,16 +1,52 @@
+
+
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.util.Enumeration;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 
 /**
  * Created by Alexey on 25.01.2017.
  */
 public class Controller extends HttpServlet {
+
+    private final String PAGECOD_ONE = "<html> <head> <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">" +
+            "<meta charset=\"utf-8\">" +
+            "<link href=\"https://fonts.googleapis.com/css?family=Roboto\" rel=\"stylesheet\">" +
+            "<meta http-equiv=\"refresh\" content=\"10; URL=./\">" +
+            "<title>HW_WebCalc</title> </head> <body> <div id=\"container\">" +
+            "<div id=\"msg_history_div\">" +
+            "<ul>";
+    private final String PAGECOD_TWO = "</ul>" +
+            "</div>" +
+            "<form action=\"./\" method=\"post\">" +
+            "<label for=\"nick\">Ник пользователя</label>" +
+            "<input id=\"nick\" required value=\"";
+    private final String PAGECOD_THREE = "\" type=\"text\" name=\"nick\" maxlength=\"50\">" +
+            "<label for=\"msg\">Сообщение</label>" +
+            "<textarea id=\"msg\" required rows=\"5\" type=\"textarea\" name=\"msg\" maxlength=\"200\"></textarea>" +
+            "<input type=\"submit\" value=\"Отправить\">" +
+            "</form>" +
+            "</div>" +
+            "<style>" +
+            "#container{ font-family: 'Roboto', sans-serif; font-size:16px; padding: 8px; border: 1px solid #dfdfdf; color: #3f3f3f; width: 360px; margin: 150px auto; }" +
+            "#msg_history_div{ height: 160px; overflow: auto; border: 1px solid #dfdfdf; }" +
+            "#msg_history_div ul{ list-style: none; margin: 8px; padding: 0; }" +
+            "#container form{ margin: 8px 0 8px 0; }" +
+            "input, textarea { width: 100%; }" +
+            "label { padding-bottom:4px; padding-top:4px; display: block; }" +
+            "input[type=\"submit\"]{ margin-top:8px; }" +
+            "</style>" +
+            "</body>" +
+            "</html>";
+
+    private JdbcCrud jdbcCrud = JdbcCrud.getInstance();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         process(req, resp);
@@ -21,54 +57,42 @@ public class Controller extends HttpServlet {
         process(req, resp);
     }
 
+    @Override
+    public void init() throws ServletException {
+        jdbcCrud.connect("h2Connection");
+    }
+
+    @Override
+    public void destroy() {
+        jdbcCrud.close();
+    }
+
     public void process(HttpServletRequest req, HttpServletResponse resp) {
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html;charset=UTF-8");
+
+        String nickName = null;
         Cookie[] cookies = req.getCookies();
-        String str = "";
-        if (cookies != null)
-        {
-            for(int i=0; i<cookies.length; i++)
-            {
-                Cookie cookie = cookies[i];
-                str = str + cookie.getValue() + ", ";
+        if (cookies!=null){
+            for (Cookie cookie: cookies){
+                if ("nickname".equals(cookie.getName())){
+                    nickName=cookie.getValue();
+                    break;
+                }
             }
         }
-        resp.setContentType( "text/html" );
- /*       String text = "<!DOCTYPE html PUBLIC " +
-                "\"-//W3C//DTD HTML 4.01 Transitional//EN\" " +
-                "\"http://www.w3.org/TR/html4/loose.dtd\"> " +
-                "<html><head>" +
-                "<meta http-equiv=\"Content-Type\" " +
-                "content=\"text/html; charset=UTF-8\"> " +
-                "<title>Пример сервлета!</title>" +
-                "</head>" +
-                "<body>" +
-                "<h1>Здравствуй, " + str + "</h1>" +
-                "</body></html>";
-
-        OutputStream outStream = null;
         try {
-            outStream = resp.getOutputStream();
-            outStream.write(text.getBytes("UTF-8"));
-            outStream.flush();
-            outStream.close();
+            resp.addCookie(new Cookie("nickname", URLEncoder.encode(nickName, "UTF-8")));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        try {
+            resp.getWriter().print(PAGECOD_ONE + jdbcCrud.read() + PAGECOD_TWO + nickName + PAGECOD_THREE);
+            resp.getWriter().close();
         } catch (IOException e) {
             e.printStackTrace();
-        }*/
-        Enumeration en = req.getParameterNames();
-        while(en.hasMoreElements()) {
-            // Get the name of the request parameter
-            String name = (String)en.nextElement();
-            out.println(name);
-
-            // Get the value of the request parameter
-            String value = req.getParameter(name);
-
-            // If the request parameter can appear more than once in the query string, get all values
-            String[] values = req.getParameterValues(name);
-
-            for (int i=0; i<values.length; i++) {
-                out.println(" " + values[i]);
-            }
         }
+
+
     }
 }
---
+++ src/main/java/JdbcCrud.java
@@ -0,0 +1,117 @@
+import java.sql.*;
+import java.util.*;
+import java.util.Date;
+
+/**
+ * Created by Alexey on 17.01.2017.
+ */
+public class JdbcCrud {
+
+    private JdbcCrud() {
+    }
+
+    private static class SingletonHelper {
+        private static final JdbcCrud SINGLETON = new JdbcCrud();
+    }
+
+    public static JdbcCrud getInstance() {
+        return JdbcCrud.SingletonHelper.SINGLETON;
+    }
+
+
+    private Connection connection;
+    private ResourceBundle bundle = ResourceBundle.getBundle("h2Connection");
+
+    public static final String CREATE = "CREATE TABLE MESSAGES (Nickname VARCHAR(127), Date DATETIME(23), Message VARCHAR(255))";
+    public static final String READ = "SELECT * FROM MESSAGES";
+    public static final String INSERT = "INSERT INTO MESSAGES(Nickname, Date, Message) VALUES(?,?,?)";
+
+
+    private PreparedStatement createStatement;
+    private PreparedStatement readStatement;
+    private PreparedStatement insertStatement;
+
+
+    public void connect(String property)  {
+        if ("postgresConnection".equals(property)) {
+            try {
+                Class.forName("org.postgresql.Driver");
+                connection = DriverManager.getConnection(bundle.getString("url"), bundle.getString("user"), bundle.getString("password"));
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } else if("h2Connection".equals(property)) {
+            try {
+                connection = DriverManager.getConnection(bundle.getString("url"));
+                createStatement = connection.prepareStatement(CREATE);
+                createStatement.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            createStatement = connection.prepareStatement(CREATE);
+            readStatement = connection.prepareStatement(READ);
+            insertStatement = connection.prepareStatement(INSERT);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void insert(String nickName, Date date, String message) {
+        try {
+            insertStatement.setString(1, nickName);
+            insertStatement.setTimestamp(2, new Timestamp(date.getTime()));
+            insertStatement.setString(3, message);
+            insertStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public String read() {
+        String html = "";
+        try (ResultSet rs = readStatement.executeQuery()) {
+            while (rs.next()) {
+                html += "<li>" + rs.getTimestamp("Nickname") + " - " + rs.getString("Date") + ": " + rs.getString("Message") + "</li>";
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return html;
+    }
+
+
+    public void close() {
+        if(createStatement != null)
+            try {
+                createStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        if(readStatement != null)
+            try {
+                readStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        if(insertStatement != null)
+            try {
+                insertStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        if(connection != null)
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+    }
+
+}
